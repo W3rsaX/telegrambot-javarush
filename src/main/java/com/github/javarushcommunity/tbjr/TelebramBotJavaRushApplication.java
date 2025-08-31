6b76a40dbc8a62eaa98ea8acf6278bac0a1d5ff2
Smirnov Egor <egor1975smirnov@gmail.com>
2025-08-31 20:59:43 +0300

    TBJR-0: added SpringBoot skeleton to the project.

---
+++ src/main/java/com/github/javarushcommunity/tbjr/TelebramBotJavaRushApplication.java
@@ -0,0 +1,13 @@
+package com.github.javarushcommunity.tbjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelebramBotJavaRushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelebramBotJavaRushApplication.class, args);
+	}
+
+}
