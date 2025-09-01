4741af178d085904abd9312ee50a5ad1ac3103f6
Smirnov Egor <egor1975smirnov@gmail.com>
2025-09-01 22:14:17 +0300

    TBJR-2: added stub telegram bot to the project.

---
+++ src/main/java/com/github/javarushcommunity/tbjr/bot/TelegramBotJavaRush.java
@@ -0,0 +1,29 @@
+package com.github.javarushcommunity.tbjr.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+
+@Component
+public class TelegramBotJavaRush  implements SpringLongPollingBot {
+
+  private final UpdateConsumer updateConsumer;
+
+  @Value("${bot.token}")
+  private String botToken;
+
+  public TelegramBotJavaRush(UpdateConsumer updateConsumer) {
+    this.updateConsumer = updateConsumer;
+  }
+
+  @Override
+  public String getBotToken() {
+    return botToken;
+  }
+
+  @Override
+  public LongPollingUpdateConsumer getUpdatesConsumer() {
+    return updateConsumer;
+  }
+}
