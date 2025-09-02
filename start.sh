4cc8264ac120172f6eac3703e863e1212d1f5750
Smirnov Egor <egor1975smirnov@gmail.com>
2025-09-02 14:28:33 +0300

    RTB-13: implement deployment process via docker

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pull new changes
+git pull
+
+# Prepare Jar
+mvn clean
+mvn package
+
+# Ensure, that docker-compose stopped
+docker-compose stop
+
+# Add environment variables
+export BOT_TOKEN=$1
+
+# Start new deployment
+docker-compose up --build -d
